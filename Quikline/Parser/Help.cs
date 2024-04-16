63e0d9522f4833d9dd334e64ddd0aac968f43215
Skypex <84551201+Skyppex@users.noreply.github.com>
2024-04-16 22:06:37 +0200

    refactor colors to use disposable

--- Quikline/Parser/Help.cs
+++ Quikline/Parser/Help.cs
@@ -4,8 +4,6 @@ internal static class Help
 {
     public static void Print(Interface @interface)
     {
-        ConsoleColor defaultColor = Console.ForegroundColor;
-
         if (@interface.Description is not null)
         {
             Console.Out.WriteLine(@interface.Description);
@@ -21,28 +19,36 @@ internal static class Help
         {
             Console.Out.WriteLine("");
             Console.Out.WriteLine("");
-            Console.ForegroundColor = ConsoleColor.DarkGreen;
-            Console.Out.Write("Arguments");
-            Console.ForegroundColor = ConsoleColor.Gray;
-            Console.Out.WriteLine(":");
 
-            for (int i = 0; i < arguments.Count; i++)
+            using (new Color(ConsoleColor.DarkGreen))
+                Console.Out.Write("Arguments");
+
+            using (new Color(ConsoleColor.Gray))
             {
-                Argument argument = arguments[i];
-                argument.PrintUsage();
+                Console.Out.WriteLine(":");
 
-                if (i < arguments.Count - 1)
-                    Console.Out.WriteLine();
+                for (int i = 0; i < arguments.Count; i++)
+                {
+                    Argument argument = arguments[i];
+                    argument.PrintUsage();
+
+                    if (i < arguments.Count - 1)
+                        Console.Out.WriteLine();
+                }
             }
         }
 
-        if (options.Count > 0)
-        {
-            Console.Out.WriteLine("");
-            Console.Out.WriteLine("");
-            Console.ForegroundColor = ConsoleColor.DarkGreen;
+        if (options.Count <= 0)
+            return;
+
+        Console.Out.WriteLine("");
+        Console.Out.WriteLine("");
+        
+        using (new Color(ConsoleColor.DarkGreen))
             Console.Out.Write("Options");
-            Console.ForegroundColor = ConsoleColor.Gray;
+
+        using (new Color(ConsoleColor.Gray))
+        {
             Console.Out.WriteLine(":");
 
             for (int i = 0; i < options.Count; i++)
@@ -54,58 +60,74 @@ internal static class Help
                     Console.Out.WriteLine();
             }
         }
-
-        Console.ForegroundColor = defaultColor;
     }
 
     private static void PrintUsage(Interface @interface)
     {
-        Console.ForegroundColor = ConsoleColor.DarkGreen;
-        Console.Out.Write("Usage");
-        Console.ForegroundColor = ConsoleColor.Gray;
-        Console.Out.Write(": ");
-        Console.Out.Write(@interface.ProgramName);
+        using (new Color(ConsoleColor.DarkGreen))
+            Console.Out.Write("Usage");
 
-        if (@interface.Options.Count > 0)
+        using (new Color(ConsoleColor.Gray))
         {
-            Console.Out.Write(" {");
-            Console.ForegroundColor = ConsoleColor.DarkCyan;
-            Console.Out.Write("options");
-            Console.ForegroundColor = ConsoleColor.Gray;
-            Console.Out.Write("}");
+            Console.Out.Write(": ");
+            Console.Out.Write(@interface.ProgramName);
+
+            if (@interface.Options.Count > 0)
+            {
+                Console.Out.Write(" {");
+                using (new Color(ConsoleColor.DarkCyan))
+                    Console.Out.Write("options");
+                Console.Out.Write("}");
+            }
         }
 
         List<Argument> arguments = @interface.Arguments;
 
         if (arguments.Count > 0)
         {
-            foreach (Argument argument in arguments)
+            using (new Color(ConsoleColor.Gray))
             {
-                Console.Out.Write(" ");
-                Console.ForegroundColor = ConsoleColor.DarkYellow;
-                
-                if (argument is { IsRest: true, RestSeparator: not null })
-                    Console.Out.Write("-- ");
-                
-                Console.ForegroundColor = ConsoleColor.Gray;
-                Console.Out.Write("<");
-
-                if (argument is { IsRest: true, RestSeparator: null })
-                    Console.Out.Write("..");
-                
-                Console.ForegroundColor = ConsoleColor.DarkCyan;
-                Console.Out.Write(argument.Name);
-                Console.ForegroundColor = ConsoleColor.Gray;
-
-                if (argument.Optional)
+                foreach (Argument argument in arguments)
                 {
-                    Console.ForegroundColor = ConsoleColor.DarkYellow;
-                    Console.Out.Write("?");
-                }
+                    Console.Out.Write(" ");
+                    
+                    if (argument is { IsRest: true, RestSeparator: not null })
+                        using (new Color(ConsoleColor.DarkYellow))
+                            Console.Out.Write("-- ");
 
-                Console.ForegroundColor = ConsoleColor.Gray;
-                Console.Out.Write(">");
+                    using (new Color(ConsoleColor.Gray))
+                    {
+                        Console.Out.Write("<");
+
+                        if (argument is { IsRest: true, RestSeparator: null })
+                            Console.Out.Write("..");
+
+                        using (new Color(ConsoleColor.DarkCyan))
+                            Console.Out.Write(argument.Name);
+
+                        if (argument.Optional)
+                        {
+                            using (new Color(ConsoleColor.DarkYellow))
+                                Console.Out.Write("?");
+                        }
+
+                        Console.Out.Write(">");
+                    }
+                }
             }
         }
+    }
+    
+    private class Color : IDisposable
+    {
+        private readonly ConsoleColor _defaultColor;
+
+        public Color(ConsoleColor color)
+        {
+            _defaultColor = Console.ForegroundColor;
+            Console.ForegroundColor = color;
+        }
+
+        public void Dispose() => Console.ForegroundColor = _defaultColor;
     }
 }
